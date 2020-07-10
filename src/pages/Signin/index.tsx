a064f4deb0246832ea8d7239488d5d26b06c5dc2
Ailton Oliveira <ailton.oliveira@vocacao.org.br>
2020-07-10 11:07:51 -0300

    create input and button components

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import LogoSvg from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Content, Background } from './styles';
 
 const Signin: React.FC = () => (
@@ -12,10 +15,15 @@ const Signin: React.FC = () => (
       <form>
         <h1>Faça seu login</h1>
 
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Senha" />
+        <Input type="text" name="email" icon={FiMail} placeholder="Email" />
+        <Input
+          type="password"
+          name="password"
+          icon={FiLock}
+          placeholder="Senha"
+        />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="link">Esqueci minha senha</a>
       </form>
