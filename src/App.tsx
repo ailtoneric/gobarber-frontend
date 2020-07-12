60e6e73d69c0d958b69c3261ff9a9431dd0d73a3
Ailton Oliveira <ailton.oliveira@vocacao.org.br>
2020-07-12 20:32:20 -0300

    create toast component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Signin from './pages/Signin';
 // import Signup from './pages/Signup';
 import GlobalStyle from './styles/global';
+import ToastContainer from './components/ToastContainer';
 
 import { AuthProvider } from './hooks/AuthContext';
 
@@ -11,6 +12,7 @@ const App: React.FC = () => (
     <AuthProvider>
       <Signin />
     </AuthProvider>
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
